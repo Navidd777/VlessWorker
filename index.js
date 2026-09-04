3801e6dd96859da03dd906e4328b4fc931eb846e
Navid777 <Navid777b@gmail.com>
2026-09-04 13:06:49 +0330

    keep local modular version (src/ modules, wrangler.jsonc, dotenv dep)

--- index.js
+++ index.js
@@ -76,6 +76,7 @@ export default {
    * @param {any} ctx
    */
   async fetch(request, env, ctx) {
+<<<<<<< Updated upstream
     //console.log("🟢 Worker started, URL:", request.url);
     try {
       const cfg = Config.fromEnv(env);
@@ -85,6 +86,19 @@ export default {
       //console.log(`[DEBUG] Request: ${request.method} ${url.pathname}`);
       //console.log(`[DEBUG] Protocol: ${url.protocol}`);
       //console.log(`[DEBUG] Headers:`, Object.fromEntries(request.headers));
+=======
+    console.log("🟢 Worker started, URL:", request.url);
+    try {
+      const cfg = Config.fromEnv(env);
+      console.log("🟢 Config loaded, userID:", cfg.userID);
+      const url = new URL(request.url);
+      console.log(`url: ${url}`, `  cfg: ${cfg}`);
+      console.log(`[DEBUG] Request: ${request.method} ${url.pathname}`);
+      console.log(`[DEBUG] Protocol: ${url.protocol}`);
+      //console.log(`[DEBUG] Headers:`, Object.fromEntries(request.headers));
+      const ConnectionHeader = request.headers.get("connection");
+      console.log(`[DEBUG] connection: `, ConnectionHeader);
+>>>>>>> Stashed changes
       const upgradeHeader = request.headers.get("Upgrade");
       //console.log("upgradeHeader:",upgradeHeader);
       
@@ -106,7 +120,12 @@ export default {
               //console.log("requestConfig: ",requestConfig ,"\n upgradeHeader: ",upgradeHeader);
               return ProtocolOverWSHandler(request, requestConfig);
       };
+<<<<<<< Updated upstream
       //if (url.pathname.startsWith(`/xhttp/${cfg.userID}`))
+=======
+      if (ConnectionHeader && ConnectionHeader.toLowerCase() === "Keep-Alive" )
+        console.log("protocol is : ..xhttp..");
+>>>>>>> Stashed changes
         //return ProtocolOverXHTTPHandler(request, requestConfig);
       if (url.pathname === "/scamalytics-lookup") {
         console.log("..scamalytics..");
